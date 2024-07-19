80e6e21e250f3cb15b631cfbb6a8f48ebf87dad5
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-07-19 12:29:35 +0300

    Outsourcing HTTP Request Logic Into A Service

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -5,6 +5,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { catchError, map, single, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-available-places',
@@ -19,49 +20,36 @@ export class AvailablePlacesComponent implements OnInit {
   isFetching = signal<boolean>(false);
   places = signal<Place[] | undefined>(undefined);
 
-  private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
+  private placesSrv = inject(PlacesService);
 
   ngOnInit() {
     this.isFetching.set(true);
-    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(map(resData => resData.places),
-        catchError(
-          (err) => {
-            console.log(err)
-            return throwError(
-              () =>
-                new Error(
-                  `Something went wrong fetching the available places. Please try again later.`
-                )
-            )
-          }
-        )
-      )
-      .subscribe(
-        {
-          next: places => {
-            this.places.set(places);
-          },
-          error: (error: Error) => {
-            this.error.set(error.message)
-          },
-          complete: () => {
-            this.isFetching.set(false);
-          }
+    const subscription = this.placesSrv.loadAvailablePlaces().subscribe(
+      {
+        next: places => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message)
+        },
+        complete: () => {
+          this.isFetching.set(false);
         }
-      );
+      }
+    );
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
   }
 
   onSelectPlace(selectedPlace: Place) {
-    this.httpClient.put('http://localhost:3000/user-places', {
-      placeId: selectedPlace.id
-    }).subscribe({
+    const subscription = this.placesSrv.addPlaceToUserPlaces(selectedPlace.id).subscribe({
       next: (resData) => console.log(resData)
     });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
   }
 
 }
--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -5,6 +5,7 @@ import { PlacesComponent } from '../places.component';
 import { Place } from '../place.model';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, throwError } from 'rxjs';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-user-places',
@@ -19,38 +20,24 @@ export class UserPlacesComponent {
   isFetching = signal<boolean>(false);
   places = signal<Place[] | undefined>(undefined);
 
-  private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
+  private placesSrv = inject(PlacesService);
 
   ngOnInit() {
     this.isFetching.set(true);
-    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/user-places')
-      .pipe(map(resData => resData.places),
-        catchError(
-          (err) => {
-            console.log(err)
-            return throwError(
-              () =>
-                new Error(
-                  `Something went wrong fetching your favorite places. Please try again later.`
-                )
-            )
-          }
-        )
-      )
-      .subscribe(
-        {
-          next: places => {
-            this.places.set(places);
-          },
-          error: (error: Error) => {
-            this.error.set(error.message)
-          },
-          complete: () => {
-            this.isFetching.set(false);
-          }
+    const subscription = this.placesSrv.loadUserPlaces().subscribe(
+      {
+        next: places => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message)
+        },
+        complete: () => {
+          this.isFetching.set(false);
         }
-      );
+      }
+    );
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
