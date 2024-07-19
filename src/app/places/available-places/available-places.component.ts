5659468067c1c6d25150cdd1441218ee65c5bdaf
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-07-19 10:54:52 +0300

    Handling HTTP Errors

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
-import { map } from 'rxjs';
+import { catchError, map, single, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -15,6 +15,7 @@ import { map } from 'rxjs';
 })
 export class AvailablePlacesComponent implements OnInit {
 
+  error = signal('');
   isFetching = signal<boolean>(false);
   places = signal<Place[] | undefined>(undefined);
 
@@ -23,14 +24,33 @@ export class AvailablePlacesComponent implements OnInit {
 
   ngOnInit() {
     this.isFetching.set(true);
-    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').pipe(map(resData => resData.places)).subscribe({
-      next: places => {
-        this.places.set(places);
-      },
-      complete: () => {
-        this.isFetching.set(false);
-      }
-    });
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(map(resData => resData.places),
+        catchError(
+          (err) => {
+            console.log(err)
+            return throwError(
+              () =>
+                new Error(
+                  `Something went wrong fetching the available places. Please try again later.`
+                )
+            )
+          }
+        )
+      )
+      .subscribe(
+        {
+          next: places => {
+            this.places.set(places);
+          },
+          error: (error: Error) => {
+            this.error.set(error.message)
+          },
+          complete: () => {
+            this.isFetching.set(false);
+          }
+        }
+      );
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
