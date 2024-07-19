6be3331db90bde07532baa3a6c0d1cc9bb884ba1
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-07-19 10:30:56 +0300

    Show loading fallback

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -13,16 +14,21 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   imports: [PlacesComponent, PlacesContainerComponent],
 })
 export class AvailablePlacesComponent implements OnInit {
+
+  isFetching = signal<boolean>(false);
   places = signal<Place[] | undefined>(undefined);
 
   private httpClient = inject(HttpClient);
-
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
-    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').subscribe({
-      next: reqData => {
-        console.log(reqData.places);
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').pipe(map(resData => resData.places)).subscribe({
+      next: places => {
+        this.places.set(places);
+      },
+      complete: () => {
+        this.isFetching.set(false);
       }
     });
     this.destroyRef.onDestroy(() => {
